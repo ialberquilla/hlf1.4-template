20230e74c2cdb36d5a6ba714b7501c43d0cd896f
Iván <ivan.alberquilla@gmail.com>
2019-11-15 21:41:47 +0100

    create dir

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Generating cryto material for peers..."
-./bin/cryptogen generate --config=./crypto-config.yaml
+../bin/cryptogen generate --config=./crypto-config.yaml
 export FABRIC_CFG_PATH=$PWD
 
+[ -d crypto-config ] || mkdir crypto-config
+
 echo "Generating channel artifacts and genesis block..."
-./bin/configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
-./bin/configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
+../bin/configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+../bin/configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
