9906980e5d9bce2e287ebf0a37c258dd5c279877
Iván <ivan.alberquilla@gmail.com>
2019-12-04 17:00:23 +0100

    tls enabled

--- scripts/channel/createChannel.sh
+++ scripts/channel/createChannel.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+CORE_PEER_TLS_ENABLED=true
+
 echo "Creating channel..."
-peer channel create -o orderer.example.com:7050 /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -c mychannel -f ./channel-artifacts/channel.tx
+peer channel create -o orderer.example.com:7050 /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -c mychannel --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -f ./channel-artifacts/channel.tx
 
 echo 
 echo "Channel created, joining Org1..."
--- scripts/channel/joinOrg2.sh
+++ scripts/channel/joinOrg2.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+CORE_PEER_LOCALMSPID=Org2MSP
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+CORE_PEER_ADDRESS=peer0.org2.example.com:8051
+CHANNEL_NAME=mychannel
+CORE_PEER_TLS_ENABLED=true
+
 echo "Joining Org2 to channel..."
 peer channel join -b mychannel.block
